8ed022b5d042b8b04f3c7571f7d3350c9b3c50e7
Алла Седорина <kornilov.s@evrogas.local>
2022-07-13 15:27:20 +0300

    Добавил 34 задачу

---
+++ Task_34/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 34: Задайте массив заполненный случайными 
+положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+
+*/
+Console.WriteLine("Введите массив ширина");
+int ShirinaMasia = Convert.ToInt32(Console.ReadLine());
+int [] Massib  = new int[ShirinaMasia];
+int count=0;
+for (int i =0; i < ShirinaMasia; i++)
+{
+    Massib[i] = new Random().Next(100,1000);
+    Console.WriteLine($"A {i} = {Massib[i]}");
+    if (Massib[i] % 2 == 0)
+    {
+        count = count +1;
+
+    }
+}
+Console.WriteLine("Колличество четных ="+ count);
